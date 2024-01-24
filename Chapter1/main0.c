170fd2c1dbc32b50515754ab7d0140ad6af19f69
usr <usr@localhost.localdomain>
2024-01-24 21:21:10 +0100

    Tried out other escape characters

--- Chapter1/main0.c
+++ Chapter1/main0.c
@@ -1,4 +1,27 @@
 #include <stdio.h>
 main (){
-  printf("Hello World\n");
+  //printf("Hello World\a\n\n");
+  printf("Hello World\b\n\n");
+  printf("Hello World\c\n\n");
+  printf("Hello World\d\n\n");
+  printf("Hello World\e\n\n");
+  printf("Hello World\f\n\n");
+  printf("Hello World\g\n\n");
+  printf("Hello World\h\n\n");
+  printf("Hello World\i\n\n");
+  printf("Hello World\j\n\n");
+  printf("Hello World\k\n\n");
+  printf("Hello World\l\n\n");
+  printf("Hello World\m\n\n");
+  printf("Hello World\o\n\n");
+  printf("Hello World\p\n\n");
+  printf("Hello World\q\n\n");
+  printf("Hello World\r\n\n");
+  printf("Hello World\s\n\n");
+  printf("Hello World\u1000 \n\n");
+  printf("Hello World\v\n\n");
+  printf("Hello World\w\n\n");
+  printf("Hello World\x100 \n\n");
+  printf("Hello World\y\n\n");
+  printf("Hello World\z\n\n");
 }
